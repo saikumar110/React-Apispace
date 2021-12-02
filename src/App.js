df1d2f4ddb2d390984930793b24caaeb7868c950
saikumar110 <sai999694@gmail.com>
2021-12-02 11:42:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 import './App.css';
 import Builder from './Components/Email_builder/Email-Body/Builder';
-import Header from "../src/Components/Email_builder/Header/Header"
-import { styled } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardActionArea from '@mui/material/CardActionArea';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { apis } from "./Config/Config"
+
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import { makeStyles } from '@mui/styles';
-import EmailBuilder from './Pages/EmailBuilder/EmailBuilder';
+// import { makeStyles } from '@mui/styles';
 import TravelAdvisor from "./Pages/TravelAdvisor/TravelAdvisor"
 import LandingPage from './Components/LandingPage';
-const useStyles = makeStyles({
-  img: {
-    padding:"10px",
-    objectFit:"cover !important",
-    width:"auto"
-  }
-})
+// const useStyles = makeStyles({
+//   img: {
+//     padding:"10px",
+//     objectFit:"cover !important",
+//     width:"auto"
+//   }
+// })
 
 
 
--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,4 +1,3 @@
-import { styled } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
